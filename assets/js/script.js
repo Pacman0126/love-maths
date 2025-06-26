34c333ce752914107ce4b4c89feb20a299325efa
Pacman0126 <oliver.p.hartmann@gmail.com>
2025-06-26 18:03:25 +0200

    add multiply code

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,7 +28,11 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
-    } else {
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    }
+
+    else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -45,9 +49,9 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
-incrementScore();
+        incrementScore();
     } else {
-        alert (`Awwww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        alert(`Awwww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
         incrementWrongAnswer();
     }
     runGame(calculatedAnswer[1]);
@@ -57,17 +61,22 @@ incrementScore();
  * directly from the dom, and returns the correct asnwer.
  */
 function calculateCorrectAnswer() {
-let operand1 = parseInt(document.getElementById('operand1').innerText);
-let operand2 = parseInt(document.getElementById('operand2').innerText);
-let operator = document.getElementById('operator').innerText;
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
 
-if (operator === "+") {
-return [operand1+operand2,"addition"];
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
 
-} else {
-    alert(`Unimplemented operator ${operator}`);
-    throw `Unimplemented operator ${operator}. Aborting!`;
-}
+    } else if (operator === "x") {
+return [operand1 * operand2, "multiply"];
+
+
+    }
+    else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
 
 }
 /**
@@ -97,6 +106,8 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
-
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
